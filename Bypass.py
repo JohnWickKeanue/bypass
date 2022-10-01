12d41677dfc55b3e10765e6198dbc63248d37f6a
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-10-01 19:04:28 +0530

    Create Bypass.py

---
+++ Bypass.py
@@ -0,0 +1,57 @@
+import time
+import cloudscraper
+from bs4 import BeautifulSoup 
+print("Everything Looks Good! Lets Continue.")
+
+url = ""  #@param {type:"string"}
+
+
+'''
+NOTE: 
+SUPPORTED DOMAINS:
+ - rocklinks.net
+ - gtlinks.me
+ - shortingly.me
+
+
+'''
+# ---------------------------------------------------------------------------------------------------------------------
+
+def bypass(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    if 'rocklinks.net' in url:
+        DOMAIN = "https://blog.disheye.com"
+    elif 'gtlinks.me' in url:
+        DOMAIN = "https://go.gyanitheme.com"
+    elif 'shortingly.me' in url:
+        DOMAIN = "https://go.techyjeeshan.xyz"
+    else:
+        DOMAIN = "https://rocklinks.net"
+
+    url = url[:-1] if url[-1] == '/' else url
+
+    code = url.split("/")[-1]
+    if 'rocklinks.net' in url:
+        final_url = f"{DOMAIN}/{code}?quelle="
+    else:
+        final_url = f"{DOMAIN}/{code}"
+
+    resp = client.get(final_url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    
+    try: inputs = soup.find(id="go-link").find_all(name="input")
+    except: return "Incorrect Link"
+    
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = { "x-requested-with": "XMLHttpRequest" }
+    
+    time.sleep(10)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+print(bypass(url))
